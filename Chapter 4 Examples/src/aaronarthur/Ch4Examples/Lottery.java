2373956214422694d9c97cc02e8d350508424524
aaron.d.arthur <aaron.d.arthur@CLC-LAB209-10.TCSD.tooelesd.org>
2018-11-07 13:10:27 -0700

    11/7

--- Chapter 4 Examples/src/aaronarthur/Ch4Examples/Lottery.java
+++ Chapter 4 Examples/src/aaronarthur/Ch4Examples/Lottery.java
@@ -10,34 +10,46 @@ public class Lottery {
 		Scanner input = new Scanner(System.in);
 		
 		// Generate a lottery number
-		int lottery = (int) (Math.random() * 100);
+		String lottery = "" + (int) (Math.random() * 10) + (int) (Math.random() * 10);
 		
+
 		// Prompt user for guess
 		System.out.print("Guess a number for the lottery (2 digits): ");
-		int guess = input.nextInt();
+		String guess = input.nextLine();
+		
+		// End program if guess isn't 2 digits
+		if (guess.length() != 2) {
+			System.out.println("Invalid input. ");
+			System.exit(1);
+		}
 		
 		// Get digits from lottery
-		int lotteryDigit1 = lottery / 10;
-		int lotteryDigit2 = lottery % 10;
+		char lotteryDigit1 = lottery.charAt(0);
+		char lotteryDigit2 = lottery.charAt(1);
 		
 		// Get digits from guess
-		int guessDigit1 = guess / 10;
-		int guessDigit2 = guess % 10;
+		char guessDigit1 = guess.charAt(0);
+		char guessDigit2 = guess.charAt(1);
 		
+		// Display lottery number
 		System.out.println("The lottery number is " + lottery);
 		
-		if (guess == lottery) {
-			System.out.println("Exact match: You win $10,000!!");
+		// Perfect match
+		if (lotteryDigit1 == guessDigit1 && lotteryDigit2 == guessDigit2) {
+			System.out.println("Exact match: You win $10,000!! ");
 		}
-		else if (guessDigit2 == lotteryDigit1 && guessDigit1 == lotteryDigit2) {
-			System.out.println("Match all digits: You win $3,000!!");
+		// Correct digits
+		else if (lotteryDigit1 == guessDigit2 && lotteryDigit2 == guessDigit1) {
+			System.out.println("Match all digits: You win $3,000!! ");
 		}
+		// One correct digit
 		else if ( guessDigit1 == lotteryDigit1
 				|| guessDigit1 == lotteryDigit2
 				|| guessDigit2 == lotteryDigit1
 				|| guessDigit2 == lotteryDigit2) {
 			System.out.println("Match one digit: You win $1,000");
 		}
+		// No match
 		else {
 			System.out.println("Sorry, no match");
 		}
